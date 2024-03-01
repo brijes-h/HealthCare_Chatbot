d3897881fbd68953214b07ee6cf79fae9605eb42
brijes-h <brijesh_1210@yahoo.com>
2024-03-01 18:35:49 +0530

    add main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import uvicorn
+from fastapi import FastAPI
+from models import *
+from executor import ResponseGPT, intentGPT
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/health-check")
+def read_root():
+    return {"Main app working": "successful"}
+
+# AI Chatbot response
+@app.post("/response")
+def chat_response(message: HumanMessage):
+    response = ResponseGPT()
+    return response.aiRespond(message)
+
+# AI Chatbot response
+@app.post("/intent")
+def chat_response(message: ForIntent):
+    response = intentGPT()
+    return response.aiRespond(message)
+
+if __name__=="__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
